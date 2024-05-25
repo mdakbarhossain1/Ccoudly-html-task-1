e55486348e1097456dcf8ce0396e15672727339f
mdakbarhossain1 <akbarshovo18@gmail.com>
2024-05-25 23:15:00 +0600

    readme md file

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Project Name
+Cloudly HTML Css Tast 1 
+## Description
+A brief description of your project goes here.
+
+## Technologies and Tools Used
+
+- **HTML**: HTML5
+- **CSS**: CSS3
+- **Bootstrap**: Bootstrap 5
+- **JavaScript Plugins**: 
+  - Swiper Js (For Slider)
+
+## Installation
+
+To use this project, follow these steps:
+
+1. Clone the repository:
+   ```sh
+   git clone https://github.com/mdakbarhossain1/Ccoudly-html-task-1
--- script.js
+++ script.js
@@ -6,4 +6,37 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("toggle");
 });
 
-console.log("first");
+// console.log("first");
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  // spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
